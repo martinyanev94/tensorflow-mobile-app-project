1c9af7fd0ffe7ae1b1cb3240b0c1922c78545e5f
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-03 00:53:54 -0500

    Add Lesson_25_Efficient_Mobilenet_Model_Retraining_For_Image_Recognition/part_4_benchmark_model_run.bash

---
+++ Lesson_25_Efficient_Mobilenet_Model_Retraining_For_Image_Recognition/part_4_benchmark_model_run.bash
@@ -0,0 +1,11 @@
+bazel build -c opt tensorflow/tools/benchmark:benchmark_model
+bazel-bin/tensorflow/tools/benchmark/benchmark_model \
+--graph=/tf_files/quantized_stripped_dogs_retrained.pb \
+--input_layer="Mul" \
+--input_layer_shape="1,299,299,3" \
+--input_layer_type="float" \
+--output_layer="final_result" \
+--show_run_order=false \
+--show_time=false \
+--show_memory=false \
+--show_summary=true
