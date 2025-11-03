35ee016d4af4508ec3d2fd4e06c682685027e190
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-03 00:54:29 -0500

    Add Lesson_2_Mobilenet_Model_Performance_Comparison/part_3_label_image_inference.bash

---
+++ Lesson_2_Mobilenet_Model_Performance_Comparison/part_3_label_image_inference.bash
@@ -0,0 +1,12 @@
+bazel-bin/tensorflow/examples/label_image/label_image \
+--graph=/tf_files/dog_retrained_mobilenet10_224.pb \
+--image=/tmp/lab1.jpg \
+--input_layer=input \
+--output_layer=final_result \
+--labels=/tf_files/dog_retrained_labels_mobilenet.txt \
+--input_height=224 \
+--input_width=224 \
+--input_mean=128 \
+--input_std=128
+n02099601 golden retriever (64): 0.144245
+n02104029 kuvasz (76): 0.0103533
