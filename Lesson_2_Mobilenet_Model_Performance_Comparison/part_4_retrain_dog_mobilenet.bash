1adbe004589875b490b61519f1dab540e5f7065d
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-03 00:54:30 -0500

    Add Lesson_2_Mobilenet_Model_Performance_Comparison/part_4_retrain_dog_mobilenet.bash

---
+++ Lesson_2_Mobilenet_Model_Performance_Comparison/part_4_retrain_dog_mobilenet.bash
@@ -0,0 +1,6 @@
+python tensorflow/examples/image_retraining/retrain.py \
+  --output_graph=/tf_files/dog_retrained_mobilenet10_224_quantized.pb \
+  --output_labels=/tf_files/dog_retrained_labels_mobilenet.txt \
+  --image_dir ~/Downloads/Images \
+  --bottleneck_dir=/tf_files/dogs_bottleneck_mobilenet \
+  --architecture mobilenet_1.0_224_quantized
