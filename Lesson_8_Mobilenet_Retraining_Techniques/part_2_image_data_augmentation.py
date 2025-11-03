0760c4af6dbd241e5f2074efce1084855ad8df4e
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-03 00:55:14 -0500

    Add Lesson_8_Mobilenet_Retraining_Techniques/part_2_image_data_augmentation.py

---
+++ Lesson_8_Mobilenet_Retraining_Techniques/part_2_image_data_augmentation.py
@@ -0,0 +1,36 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# Creating a data generator for loading images
+train_datagen = ImageDataGenerator(
+    rescale=1.0/255,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+# Create generators for training and validation datasets
+train_generator = train_datagen.flow_from_directory(
+    'data/train',
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+validation_generator = train_datagen.flow_from_directory(
+    'data/validation',
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='categorical'
+)
+# Training the model
+history = final_model.fit(
+    train_generator,
+    steps_per_epoch=train_generator.samples // train_generator.batch_size,
+    validation_data=validation_generator,
+    validation_steps=validation_generator.samples // validation_generator.batch_size,
+    epochs=10
+)
