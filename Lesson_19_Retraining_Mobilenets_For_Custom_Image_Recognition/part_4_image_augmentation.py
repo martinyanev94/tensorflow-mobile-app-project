8a0b7a95105e557060d8bf442d606a443787e6d6
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-03 00:53:18 -0500

    Add Lesson_19_Retraining_Mobilenets_For_Custom_Image_Recognition/part_4_image_augmentation.py

---
+++ Lesson_19_Retraining_Mobilenets_For_Custom_Image_Recognition/part_4_image_augmentation.py
@@ -0,0 +1,14 @@
+from keras.preprocessing.image import ImageDataGenerator
+
+datagen = ImageDataGenerator(
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+# Assuming X_train contains your training images
+datagen.fit(X_train)
